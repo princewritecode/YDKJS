105189e02702bdbc0c39d4c07f8aa3c8e18a3ad8
princewritecode <princewritecode@gmail.com>
2023-08-31 00:04:37 +0530

    understanding callback deep

--- conceptBuilding/script.js
+++ conceptBuilding/script.js
@@ -95,3 +95,20 @@ function letScoping() {
     }
     console.log(x); // will print 1
 }
+
+
+
+//** callback functions */
+
+function modifyArray(arr, callback) {
+    // do something to arr here
+    arr.push(100);
+    // then execute the callback function that was passed
+    callback();
+}
+
+var arr = [1, 2, 3, 4, 5];
+
+modifyArray(arr, function () {
+    console.log("array has been modified", arr);
+});
