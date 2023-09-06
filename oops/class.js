80660cd1e6c3845f835224aa30ef3aa06676318f
princewritecode <princewritecode@gmail.com>
2023-09-06 20:44:17 +0530

    Class super keyword concept

--- oops/class.js
+++ oops/class.js
@@ -27,7 +27,7 @@ console.log(person2);
 
 */
 
-
+/*
 class Rectangle {
     constructor(_height, _width) {
         this.rectheight = _height;
@@ -44,3 +44,39 @@ newRectangle;
 const newRectangle2 = new Rectangle(12, 13);
 newRectangle2;
 console.log(newRectangle2.getArea());
+*/
+
+class Parent {
+
+    constructor(_name, _age, _salary) {
+        this.name = _name;
+        this.age = _age;
+        this.salary = _salary;
+        this.someFunction = function () {
+            console.log("hello");
+        };
+        console.log(this.name, this.age, this.salary);
+    };
+    talk() {
+        console.log(` ${this.name} is talking`);
+    }
+}
+
+
+class Child extends Parent {
+
+    constructor(_birthday, _birthyear, _category) {
+        super("prince", 27, 444444440000000000);
+        this.birthday = _birthday;
+        this.birthyear = _birthyear;
+        this.category = _category;
+    }
+    speak() {
+        console.log(`${this.name} will do ${this.category}`);
+    }
+    childFunc() { }
+};
+const children = new Child(24, 1996, "OBC");
+
+console.log(children);
+children.speak();
