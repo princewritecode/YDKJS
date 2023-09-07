fba13bfe0353c786ad8aa7ef02288af8bd3781aa
princewritecode <princewritecode@gmail.com>
2023-09-07 10:37:26 +0530

    understanding constructor function

--- oops/class.js
+++ oops/class.js
@@ -46,6 +46,7 @@ newRectangle2;
 console.log(newRectangle2.getArea());
 */
 
+/*
 class Parent {
 
     constructor(_name, _age, _salary) {
@@ -62,7 +63,6 @@ class Parent {
     }
 }
 
-
 class Child extends Parent {
 
     constructor(_birthday, _birthyear, _category) {
@@ -72,7 +72,7 @@ class Child extends Parent {
         this.category = _category;
     }
     speak() {
-        console.log(`${this.name} will do ${this.category}`);
+        console.log(`${this.name} will do ${this.category} and salary ${this.salary}`);
     }
     childFunc() { }
 };
@@ -80,3 +80,45 @@ const children = new Child(24, 1996, "OBC");
 
 console.log(children);
 children.speak();
+
+*/
+/*
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    const currentDate = new Date;
+    this.calcAge = function () {
+        console.log(` ${currentDate.getUTCFullYear() - this.birthYear} `);
+    };
+};
+const prince = new Person("prince", 1996);
+prince.calcAge();
+const patel = new Person("patel", 1998);
+patel.calcAge();
+
+Person.prototype.calculateAge = function () {
+    const currentDate = new Date;
+    console.log(` ${currentDate.getUTCFullYear() - this.birthYear} `);
+};
+
+const tanish = new Person("tanish", 1997);
+tanish.calculateAge();
+
+console.log(Person.prototype == tanish.__proto__);
+*/
+
+
+function Car(speed) {
+    this.speed = speed;
+}
+
+Car.prototype.accelerate = function () {
+    console.log(`Increase speed by 10 ${this.speed + 10}`);
+};
+Car.prototype.brake = function () {
+    console.log(`Decrease speed by 5 ${this.speed - 5}`);
+};
+
+const bmw = new Car(120);
+bmw.accelerate();
+bmw.brake();
