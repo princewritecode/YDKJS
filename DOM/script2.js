42cae668cd92b5c431959cbaeb197c708ae75d06
princewritecode <princewritecode@gmail.com>
2023-09-16 12:47:28 +0530

    understanding dom deep

---
+++ DOM/script2.js
@@ -0,0 +1,36 @@
+/*
+function capitalize(string) {
+
+    let arrFromString = string.split("");
+    for (let i in arrFromString) {
+        console.log(i);
+        if (i % 2 != 0) {
+            console.log(arrFromString[i] = arrFromString[i].toUpperCase());
+        }
+    }
+    console.log(arrFromString);
+}
+capitalize("prince")
+
+*/
+
+/*
+const heading = document.createElement("h1");
+heading.innerText = "hello world ";
+const body = document.getElementsByTagName('body')[0];
+body.appendChild(heading);
+*/
+
+/*
+let btn = document.querySelector('#btn');
+
+btn.addEventListener('click', function (event) {
+    console.log('The button was clicked!');
+    event.stopPropagation();
+});
+
+document.body.addEventListener('click', function (event) {
+    console.log('The body was clicked!');
+});
+
+*/
